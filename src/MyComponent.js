ae4e2a5a7b434203ed3c45f9fc46e1d456079e28
Antonio Barbosa <albedoarte@gmail.com>
2025-03-17 18:06:52 -0700

    addition of mycomponent file

---
+++ src/MyComponent.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+
+function MyComponent() {
+    const paragraph = 'HELLO WORLD!'
+    return(
+        <div>
+            <h1>{paragraph}</h1>
+        </div>
+
+    );
+};
+
+export default MyComponent
